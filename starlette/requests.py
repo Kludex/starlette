9ada6312338c85a952359453de024ef6c5c12c92
11kkw <11kkw@naver.com>
2025-09-11 22:12:26 +0900

    refactor: let cookie_parser accept list of headers

--- starlette/requests.py
+++ starlette/requests.py
@@ -37,29 +37,34 @@ SERVER_PUSH_HEADERS_TO_COPY = {
 }
 
 
-def cookie_parser(cookie_string: str) -> dict[str, str]:
+def cookie_parser(raw: list[str]) -> dict[str, str]:
     """
-    This function parses a ``Cookie`` HTTP header into a dict of key/value pairs.
+    Parse ``Cookie`` HTTP headers into a dict of key/value pairs.
 
-    It attempts to mimic browser cookie parsing behavior: browsers and web servers
-    frequently disregard the spec (RFC 6265) when setting and reading cookies,
-    so we attempt to suit the common scenarios here.
+    Accepts a list of cookie header strings (e.g. multiple "cookie" headers in HTTP/2).
+    The headers are joined into a single cookie string before parsing.
 
-    This function has been adapted from Django 3.1.0.
-    Note: we are explicitly _NOT_ using `SimpleCookie.load` because it is based
-    on an outdated spec and will fail on lots of input we want to support
+    This function attempts to mimic browser cookie parsing behavior: browsers and
+    web servers frequently disregard the spec (RFC 6265) when setting and reading
+    cookies, so we attempt to handle the most common scenarios here.
+
+    This implementation has been adapted from Django 3.1.0.
+    Note: we are explicitly *not* using ``SimpleCookie.load`` because it is based
+    on an outdated spec and will fail on many inputs we want to support.
     """
+    raw_str = "; ".join(value.rstrip("; ") for value in raw if value)
+
     cookie_dict: dict[str, str] = {}
-    for chunk in cookie_string.split(";"):
+    for chunk in raw_str.split(";"):
         if "=" in chunk:
             key, val = chunk.split("=", 1)
         else:
-            # Assume an empty name per
+            # Assume an empty name per:
             # https://bugzilla.mozilla.org/show_bug.cgi?id=169091
             key, val = "", chunk
         key, val = key.strip(), val.strip()
         if key or val:
-            # unquote using Python's algorithm.
+            # Unquote using Python's algorithm.
             cookie_dict[key] = http_cookies._unquote(val)
     return cookie_dict
 
@@ -143,8 +148,7 @@ class HTTPConnection(Mapping[str, Any]):
             cookie_headers = self.headers.getlist("cookie")
 
             if cookie_headers:
-                joined = "; ".join(value.rstrip("; ") for value in cookie_headers if value)
-                cookies = cookie_parser(joined)
+                cookies = cookie_parser(cookie_headers)
 
             self._cookies = cookies
         return self._cookies

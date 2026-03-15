e33421c989857c7c2dc368dc16d94db8f8030167
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-03-15 15:21:44 +0100

    Use private instance attributes for resolved parser limits

--- starlette/formparsers.py
+++ starlette/formparsers.py
@@ -165,10 +165,8 @@ class MultiPartParser:
         self._file_parts_to_write: list[tuple[MultipartPart, bytes]] = []
         self._file_parts_to_finish: list[MultipartPart] = []
         self._files_to_close_on_error: list[SpooledTemporaryFile[bytes]] = []
-        if max_part_size is not _Unset:
-            self.max_part_size = max_part_size
-        if spool_max_size is not _Unset:
-            self.spool_max_size = spool_max_size
+        self._max_part_size = max_part_size if max_part_size is not _Unset else type(self).max_part_size
+        self._spool_max_size = spool_max_size if spool_max_size is not _Unset else type(self).spool_max_size
 
     def on_part_begin(self) -> None:
         self._current_part = MultipartPart()
@@ -176,8 +174,8 @@ class MultiPartParser:
     def on_part_data(self, data: bytes, start: int, end: int) -> None:
         message_bytes = data[start:end]
         if self._current_part.file is None:
-            if len(self._current_part.data) + len(message_bytes) > self.max_part_size:
-                raise MultiPartException(f"Part exceeded maximum size of {int(self.max_part_size / 1024)}KB.")
+            if len(self._current_part.data) + len(message_bytes) > self._max_part_size:
+                raise MultiPartException(f"Part exceeded maximum size of {int(self._max_part_size / 1024)}KB.")
             self._current_part.data.extend(message_bytes)
         else:
             self._file_parts_to_write.append((self._current_part, message_bytes))
@@ -222,7 +220,7 @@ class MultiPartParser:
             if self._current_files > self.max_files:
                 raise MultiPartException(f"Too many files. Maximum number of files is {self.max_files}.")
             filename = _user_safe_decode(options[b"filename"], self._charset)
-            tempfile = SpooledTemporaryFile(max_size=self.spool_max_size)
+            tempfile = SpooledTemporaryFile(max_size=self._spool_max_size)
             self._files_to_close_on_error.append(tempfile)
             self._current_part.file = UploadFile(
                 file=tempfile,  # type: ignore[arg-type]

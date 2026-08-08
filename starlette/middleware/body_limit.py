0115e6395d2f7b266ec6152a23e5263fdb518c28
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-08-08 17:41:01 +0200

    Use a responder for request body limits

--- starlette/middleware/body_limit.py
+++ starlette/middleware/body_limit.py
@@ -8,7 +8,7 @@ from starlette.responses import PlainTextResponse
 from starlette.types import ASGIApp, Message, Receive, Scope, Send
 
 MAX_BODY_SIZE_SCOPE_KEY = "starlette.max_body_size"
-_BODY_SIZE_STATE_SCOPE_KEY = "starlette._body_size_state"
+_BODY_LIMIT_RESPONDER_SCOPE_KEY = "starlette._body_limit_responder"
 _MISSING = object()
 
 
@@ -19,14 +19,6 @@ class _RequestBodyTooLarge(HTTPException):
 
 class _RequestBodyLimitResponseSent(Exception):
     pass
-
-
-class _BodySizeState:
-    def __init__(self, content_length: int | None) -> None:
-        self.content_length = content_length
-        self.total_size = 0
-        self.max_body_size = 0
-        self.response_started = False
 
 
 class RequestBodyLimitMiddleware:
@@ -42,76 +34,82 @@ class RequestBodyLimitMiddleware:
         if scope["type"] != "http":
             return await self.app(scope, receive, send)
 
+        responder = RequestBodyLimitResponder(self.app, self.max_body_size)
+        await responder(scope, receive, send)
+
+
+class RequestBodyLimitResponder:
+    def __init__(self, app: ASGIApp, max_body_size: int) -> None:
+        self.app = app
+        self.max_body_size = max_body_size
+        self.scope: Scope | None = None
+        self.receive: Receive | None = None
+        self.send: Send | None = None
+        self.content_length: int | None = None
+        self.total_size = 0
+        self.response_started = False
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
         previous_scope_limit: Any = scope.get(MAX_BODY_SIZE_SCOPE_KEY, _MISSING)
         scope[MAX_BODY_SIZE_SCOPE_KEY] = self.max_body_size
 
-        state = cast(_BodySizeState | None, scope.get(_BODY_SIZE_STATE_SCOPE_KEY))
-        is_outermost = state is None
-        if is_outermost:
-            content_length: int | None = None
-            content_length_header = Headers(scope=scope).get("content-length")
-            if content_length_header is not None:
-                try:
-                    content_length = int(content_length_header)
-                except ValueError:
-                    pass
-            state = _BodySizeState(content_length)
-            scope[_BODY_SIZE_STATE_SCOPE_KEY] = state
+        active_responder = cast(RequestBodyLimitResponder | None, scope.get(_BODY_LIMIT_RESPONDER_SCOPE_KEY))
+        if active_responder is not None:
+            active_responder.max_body_size = self.max_body_size
+            if active_responder.total_size > active_responder.max_body_size:
+                raise _RequestBodyTooLarge
+            await self.app(scope, receive, send)
+            return
 
-        assert state is not None
-
-        state.max_body_size = self.max_body_size
+        self.scope = scope
+        self.receive = receive
+        self.send = send
+        content_length_header = Headers(scope=scope).get("content-length")
+        if content_length_header is not None:
+            try:
+                self.content_length = int(content_length_header)
+            except ValueError:
+                pass
+        scope[_BODY_LIMIT_RESPONDER_SCOPE_KEY] = self
 
         try:
-            if state.total_size > state.max_body_size:
-                raise _RequestBodyTooLarge
-
-            if is_outermost:
-                await self._run_with_limit(scope, receive, send, state)
-            else:
-                await self.app(scope, receive, send)
+            await self.app(scope, self.receive_with_limit, self.send_with_limit)
         except _RequestBodyTooLarge:
-            if not is_outermost or state.response_started:
+            if self.response_started:
                 raise
             response = PlainTextResponse("Content Too Large", status_code=413)
             await response(scope, receive, send)
         except _RequestBodyLimitResponseSent:
-            if not is_outermost:
-                raise
+            pass
         finally:
-            if is_outermost:
-                scope.pop(_BODY_SIZE_STATE_SCOPE_KEY, None)
-                if previous_scope_limit is _MISSING:
-                    scope.pop(MAX_BODY_SIZE_SCOPE_KEY, None)
-                else:
-                    scope[MAX_BODY_SIZE_SCOPE_KEY] = previous_scope_limit
+            scope.pop(_BODY_LIMIT_RESPONDER_SCOPE_KEY, None)
+            if previous_scope_limit is _MISSING:
+                scope.pop(MAX_BODY_SIZE_SCOPE_KEY, None)
+            else:
+                scope[MAX_BODY_SIZE_SCOPE_KEY] = previous_scope_limit
 
-    async def _run_with_limit(
-        self,
-        scope: Scope,
-        receive: Receive,
-        send: Send,
-        state: _BodySizeState,
-    ) -> None:
-        async def limited_receive() -> Message:
-            if state.content_length is not None and state.content_length > state.max_body_size:
+    async def receive_with_limit(self) -> Message:
+        if self.content_length is not None and self.content_length > self.max_body_size:
+            raise _RequestBodyTooLarge
+
+        assert self.receive is not None
+        message = await self.receive()
+        if message["type"] == "http.request":
+            self.total_size += len(message.get("body", b""))
+            if self.total_size > self.max_body_size:
                 raise _RequestBodyTooLarge
+        return message
 
-            message = await receive()
-            if message["type"] == "http.request":
-                state.total_size += len(message.get("body", b""))
-                if state.total_size > state.max_body_size:
-                    raise _RequestBodyTooLarge
-            return message
+    async def send_with_limit(self, message: Message) -> None:
+        assert self.scope is not None
+        assert self.receive is not None
+        assert self.send is not None
 
-        async def tracked_send(message: Message) -> None:
-            if message["type"] == "http.response.start":
-                if state.content_length is not None and state.content_length > state.max_body_size:
-                    state.response_started = True
-                    response = PlainTextResponse("Content Too Large", status_code=413)
-                    await response(scope, receive, send)
-                    raise _RequestBodyLimitResponseSent
-                state.response_started = True
-            await send(message)
-
-        await self.app(scope, limited_receive, tracked_send)
+        if message["type"] == "http.response.start":
+            if self.content_length is not None and self.content_length > self.max_body_size:
+                self.response_started = True
+                response = PlainTextResponse("Content Too Large", status_code=413)
+                await response(self.scope, self.receive, self.send)
+                raise _RequestBodyLimitResponseSent
+            self.response_started = True
+        await self.send(message)

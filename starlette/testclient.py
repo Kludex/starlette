6a9a645c70f5b7cbeed86283439180112b4001ee
Marcelo Trylesinski <marcelotryle@gmail.com>
2026-09-05 10:11:57 +0200

    Drop support for AnyIO 3

--- starlette/testclient.py
+++ starlette/testclient.py
@@ -7,7 +7,7 @@ import json
 import math
 import sys
 import warnings
-from collections.abc import Awaitable, Callable, Generator, Iterable, Mapping, MutableMapping, Sequence
+from collections.abc import Awaitable, Callable, Generator, Iterable, Mapping, Sequence
 from concurrent.futures import Future
 from contextlib import AbstractContextManager
 from types import GeneratorType
@@ -134,10 +134,8 @@ class WebSocketTestSession:
         """
         The sub-thread in which the websocket session runs.
         """
-        send: anyio.create_memory_object_stream[Message] = anyio.create_memory_object_stream(math.inf)
-        send_tx, send_rx = send
-        receive: anyio.create_memory_object_stream[Message] = anyio.create_memory_object_stream(math.inf)
-        receive_tx, receive_rx = receive
+        send_tx, send_rx = anyio.create_memory_object_stream[Message](math.inf)
+        receive_tx, receive_rx = anyio.create_memory_object_stream[Message](math.inf)
         with send_tx, send_rx, receive_tx, receive_rx, anyio.CancelScope() as cs:
             self._receive_tx = receive_tx
             self._send_rx = send_rx
@@ -493,12 +491,8 @@ class TestClient(httpx.Client):
             def reset_portal() -> None:
                 self.portal = None
 
-            send: anyio.create_memory_object_stream[MutableMapping[str, Any] | None] = (
-                anyio.create_memory_object_stream(math.inf)
-            )
-            receive: anyio.create_memory_object_stream[MutableMapping[str, Any]] = anyio.create_memory_object_stream(
-                math.inf
-            )
+            send = anyio.create_memory_object_stream[Message | None](math.inf)
+            receive = anyio.create_memory_object_stream[Message](math.inf)
             for channel in (*send, *receive):
                 stack.callback(channel.close)
             self.stream_send = StapledObjectStream(*send)
